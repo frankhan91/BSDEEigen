e91456de6bcc188cb129b885f06923f5992b9374
Jiequn Han <frankhan91@gmail.com>
2019-01-13 14:39:48 -0500

    Simplify codes of network -- only two networks are used for y and z.

--- equation.py
+++ equation.py
@@ -5,14 +5,13 @@ from scipy.stats import multivariate_normal as normal
 
 class Equation(object):
     """Base class for defining PDE related function."""
-
     def __init__(self, eqn_config):
-        self._dim = eqn_config.dim
-        self._total_time = eqn_config.total_time
-        self._num_time_interval = eqn_config.num_time_interval
-        self._delta_t = (self._total_time + 0.0) / self._num_time_interval
-        self._sqrt_delta_t = np.sqrt(self._delta_t)
-        self._eigen = None
+        self.dim = eqn_config.dim
+        self.total_time = eqn_config.total_time
+        self.num_time_interval = eqn_config.num_time_interval
+        self.delta_t = (self.total_time + 0.0) / self.num_time_interval
+        self.sqrt_delta_t = np.sqrt(self.delta_t)
+        self.eigen = None
 
     def sample(self, num_sample):
         """Sample forward SDE."""
@@ -20,49 +19,36 @@ class Equation(object):
 
     def f_tf(self, x, y, z):
         """Generator function in the PDE."""
-        return self._eigen * y
+        return self.eigen * y
 
     def g_tf(self, x):
         """Terminal condition of the PDE."""
         raise NotImplementedError
 
-    @property
-    def dim(self):
-        return self._dim
-
-    @property
-    def num_time_interval(self):
-        return self._num_time_interval
-
-    @property
-    def total_time(self):
-        return self._total_time
-
-    @property
-    def delta_t(self):
-        return self._delta_t
-
 
 class LaplacianEigen(Equation):
+    # eigenvalue problem for Laplacian operator on squares [0, 2pi]^d
     def __init__(self, eqn_config):
         super(LaplacianEigen, self).__init__(eqn_config)
-        self._sigma = np.sqrt(2.0)
-        self._eigen_array = np.reshape(np.array(eqn_config.eigen_array), [1, self._dim])
-        self._eigen = np.sum(self._eigen_array**2)
+        self.sigma = np.sqrt(2.0)
+        self.eigen_array = np.reshape(np.array(eqn_config.eigen_array), [1, self.dim])
+        self.eigen = np.sum(self.eigen_array ** 2)
+        # self._const_fac = np.sqrt(2.0) ** self._dim
+        self.const_fac = 1 / ((np.pi / 2) ** self.dim)
 
     def sample(self, num_sample):
         dw_sample = normal.rvs(size=[num_sample,
-                                     self._dim,
-                                     self._num_time_interval]) * self._sqrt_delta_t
-        x_sample = np.zeros([num_sample, self._dim, self._num_time_interval + 1])
-        x_sample[:, :, 0] = np.random.uniform(0.0, np.pi, size=[num_sample, self._dim])
-        for i in range(self._num_time_interval):
-            x_sample[:, :, i + 1] = x_sample[:, :, i] + self._sigma * dw_sample[:, :, i]
+                                     self.dim,
+                                     self.num_time_interval]) * self.sqrt_delta_t
+        x_sample = np.zeros([num_sample, self.dim, self.num_time_interval + 1])
+        x_sample[:, :, 0] = np.random.uniform(0.0, np.pi, size=[num_sample, self.dim])
+        for i in range(self.num_time_interval):
+            x_sample[:, :, i + 1] = x_sample[:, :, i] + self.sigma * dw_sample[:, :, i]
         return dw_sample, x_sample
 
     def g_tf(self, x):
-        return tf.reduce_prod(tf.sin(self._eigen_array*x), axis=1, keepdims=True) / (np.pi/2) ** self._dim
+        return tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True) * self.const_fac
 
     def true_z(self, x):
-        prod = tf.reduce_prod(tf.sin(self._eigen_array*x), axis=1, keepdims=True) / (np.pi/2) ** self._dim
-        return prod / tf.sin(self._eigen_array*x) * tf.cos(self._eigen_array*x) * self._eigen_array * self._sigma
+        prod = tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True) * self.const_fac
+        return prod / tf.tan(self.eigen_array * x) * self.eigen_array * self.sigma
--- solver.py
+++ solver.py
@@ -12,160 +12,159 @@ DELTA_CLIP = 50.0
 class FeedForwardModel(object):
     """The fully connected neural network model."""
     def __init__(self, config, bsde, sess):
-        self._eqn_config = config.eqn_config
-        self._nn_config = config.nn_config
-        self._bsde = bsde
-        self._sess = sess
+        self.eqn_config = config.eqn_config
+        self.nn_config = config.nn_config
+        self.bsde = bsde
+        self.sess = sess
         # make sure consistent with FBSDE equation
-        self._dim = bsde.dim
-        self._num_time_interval = bsde.num_time_interval
-        self._total_time = bsde.total_time
+        self.dim = bsde.dim
+        self.num_time_interval = bsde.num_time_interval
+        self.total_time = bsde.total_time
         # ops for statistics update of batch normalization
-        self._extra_train_ops = []
-        self._init_coeff = tf.constant([1.0])
+        self.extra_train_ops = []
+        self.dw = tf.placeholder(TF_DTYPE, [None, self.dim, self.num_time_interval], name='dW')
+        self.x = tf.placeholder(TF_DTYPE, [None, self.dim, self.num_time_interval + 1], name='X')
+        self.init_coeff = tf.constant([1.0])
+        self.train_loss, self.init_loss, self.init_rel_loss = None, None, None
+        self.train_ops, self.t_build = None, None
 
     def train(self):
         start_time = time.time()
         # to save iteration results
         training_history = []
         # for validation
-        dw_valid, x_valid = self._bsde.sample(self._nn_config.valid_size)
+        dw_valid, x_valid = self.bsde.sample(self.nn_config.valid_size)
         # can still use batch norm of samples in the validation phase
-        feed_dict_valid = {self._dw: dw_valid, self._x: x_valid, self._is_training: False}
+        feed_dict_valid = {self.dw: dw_valid, self.x: x_valid}
         # initialization
-        self._sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.global_variables_initializer())
         # begin sgd iteration
-        for step in range(self._nn_config.num_iterations+1):
-            if step % self._nn_config.logging_frequency == 0:
-                train_loss, init_loss, init_rel_loss, init_coeff = self._sess.run(
-                    [self._train_loss, self._init_loss, self._init_rel_loss, self._init_coeff],
+        for step in range(self.nn_config.num_iterations+1):
+            if step % self.nn_config.logging_frequency == 0:
+                train_loss, init_loss, init_rel_loss, init_coeff = self.sess.run(
+                    [self.train_loss, self.init_loss, self.init_rel_loss, self.init_coeff],
                     feed_dict=feed_dict_valid)
-                elapsed_time = time.time()-start_time+self._t_build
+                elapsed_time = time.time()-start_time+self.t_build
                 training_history.append([step, train_loss, init_loss, init_rel_loss, elapsed_time])
-                if self._nn_config.verbose:
+                if self.nn_config.verbose:
                     logging.info(
-                        "step: %5u,    train_loss: %.4e,   init_loss: %.4e, init_rel_loss: %.4e,  init_coeff: %.2f   elapsed time %3u" % (
-                        step, train_loss, init_loss, init_rel_loss, init_coeff, elapsed_time))
-            dw_train, x_train = self._bsde.sample(self._nn_config.batch_size)
-            self._sess.run(self._train_ops, feed_dict={self._dw: dw_train,
-                                                       self._x: x_train,
-                                                       self._is_training: True})
+                        "step: %5u,    train_loss: %.4e,   init_loss: %.4e, " % (
+                            step, train_loss, init_loss) +
+                        "init_rel_loss: %.4e,  init_coeff: %.2f   elapsed time %3u" % (
+                         init_rel_loss, init_coeff, elapsed_time))
+            dw_train, x_train = self.bsde.sample(self.nn_config.batch_size)
+            self.sess.run(self.train_ops, feed_dict={self.dw: dw_train, self.x: x_train})
         return np.array(training_history)
 
     def build(self):
         start_time = time.time()
-        self._dw = tf.placeholder(TF_DTYPE, [None, self._dim, self._num_time_interval], name='dW')
-        self._x = tf.placeholder(TF_DTYPE, [None, self._dim, self._num_time_interval + 1], name='X')
-        self._is_training = tf.placeholder(tf.bool)
-        num_hiddens = self._nn_config.num_hiddens.copy()
-        num_hiddens[-1] = 1  # the dimension of y_init is 1 rather than dim
-        x_init = self._x[:, :, 0]
-        self._y_init = self._subnetwork(x_init, num_hiddens, 'y_init')
-        grad_init = tf.gradients(self._y_init, x_init)[0]
-        y = self._y_init
-        z = grad_init
         with tf.variable_scope('forward'):
-            for t in range(0, self._num_time_interval-1):
-                if t == 0:
-                    reuse = False
-                else:
-                    reuse = True
-                y = y - self._bsde.delta_t * self._bsde.f_tf(self._x[:, :, t], y, z) + \
-                    tf.reduce_sum(z * self._dw[:, :, t], 1, keepdims=True)
-                z = self._subnetwork(
-                    self._x[:, :, t + 1], self._nn_config.num_hiddens,
-                    name="z_net", reuse=reuse)
+            x_init = self.x[:, :, 0]
+            net_y = PeriodNet(self.nn_config.num_hiddens, out_dim=1, name='net_y')
+            net_z = PeriodNet(self.nn_config.num_hiddens, out_dim=2, name='net_z')
+            y_init = net_y(x_init)
+            z = net_z(x_init)
+            y = y_init
+            for t in range(0, self.num_time_interval-1):
+                y = y - self.bsde.delta_t * self.bsde.f_tf(self.x[:, :, t], y, z) + \
+                    tf.reduce_sum(z * self.dw[:, :, t], 1, keepdims=True)
+                z = net_z(self.x[:, :, t + 1], reuse=True)
             # terminal time
-            y = y - self._bsde.delta_t * self._bsde.f_tf(self._x[:, :, -2], y, z) + \
-                tf.reduce_sum(z * self._dw[:, :, -1], 1, keepdims=True)
-            delta = y - self._bsde.g_tf(self._x[:, :, -1])
+            y = y - self.bsde.delta_t * self.bsde.f_tf(self.x[:, :, -2], y, z) + \
+                tf.reduce_sum(z * self.dw[:, :, -1], 1, keepdims=True)
+            delta = y - self.bsde.g_tf(self.x[:, :, -1])
             # use linear approximation outside the clipped range
-            self._train_loss = tf.reduce_mean(
+            self.train_loss = tf.reduce_mean(
                 tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),
                          2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2)) * 10
         # f_tf also gives the true eigenfunction
-        true_init = self._bsde.g_tf(self._x[:, :, 0])
+        true_init = self.bsde.g_tf(self.x[:, :, 0])
         mask = tf.greater(tf.abs(true_init), 0.1)
-        rel_err = tf.abs((self._y_init - true_init) / true_init)
+        rel_err = tf.abs((y_init - true_init) / true_init)
         rel_err = tf.boolean_mask(rel_err, mask)
-        self._init_rel_loss = tf.reduce_mean(rel_err)
-        self._init_loss = tf.reduce_mean((true_init - self._y_init) ** 2)
+        self.init_rel_loss = tf.reduce_mean(rel_err)
+        self.init_loss = tf.reduce_mean((true_init - y_init) ** 2)
 
         # train operations
         global_step = tf.get_variable('global_step', [],
                                       initializer=tf.constant_initializer(0),
                                       trainable=False, dtype=tf.int32)
         learning_rate = tf.train.piecewise_constant(global_step,
-                                                    self._nn_config.lr_boundaries,
-                                                    self._nn_config.lr_values)
+                                                    self.nn_config.lr_boundaries,
+                                                    self.nn_config.lr_values)
         trainable_variables = tf.trainable_variables()
-        grads = tf.gradients(self._train_loss, trainable_variables)
+        grads = tf.gradients(self.train_loss, trainable_variables)
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         apply_op = optimizer.apply_gradients(zip(grads, trainable_variables),
                                              global_step=global_step, name='train_step')
-        all_ops = [apply_op] + self._extra_train_ops
-        self._train_ops = tf.group(*all_ops)
-        self._t_build = time.time()-start_time
+        all_ops = [apply_op] + self.extra_train_ops
+        self.train_ops = tf.group(*all_ops)
+        self.t_build = time.time() - start_time
 
     def build_true(self):
         start_time = time.time()
-        self._dw = tf.placeholder(TF_DTYPE, [None, self._dim, self._num_time_interval], name='dW')
-        self._x = tf.placeholder(TF_DTYPE, [None, self._dim, self._num_time_interval + 1], name='X')
-        self._is_training = tf.placeholder(tf.bool)
-        self._init_coeff = tf.get_variable(
-            'init_coeff', [1], TF_DTYPE, initializer=tf.constant_initializer(0.8, TF_DTYPE),
-            trainable=True)
-        self._y_init = self._bsde.g_tf(self._x[:, :, 0]) * self._init_coeff
-        y = self._y_init
-        z = self._bsde.true_z(self._x[:, :, 0])
         with tf.variable_scope('forward'):
-            for t in range(0, self._num_time_interval-1):
-                y = y - self._bsde.delta_t * self._bsde.f_tf(self._x[:, :, t], y, z) + \
-                    tf.reduce_sum(z * self._dw[:, :, t], 1, keepdims=True)
-                z = self._bsde.true_z(self._x[:, :, t+1])
+            self.init_coeff = tf.get_variable(
+                'init_coeff', [1], TF_DTYPE, initializer=tf.constant_initializer(0.8, TF_DTYPE),
+                trainable=True)
+            y_init = self.bsde.g_tf(self.x[:, :, 0]) * self.init_coeff
+            y = y_init
+            z = self.bsde.true_z(self.x[:, :, 0])
+            for t in range(0, self.num_time_interval-1):
+                y = y - self.bsde.delta_t * self.bsde.f_tf(self.x[:, :, t], y, z) + \
+                    tf.reduce_sum(z * self.dw[:, :, t], 1, keepdims=True)
+                z = self.bsde.true_z(self.x[:, :, t + 1])
             # terminal time
-            y = y - self._bsde.delta_t * self._bsde.f_tf(self._x[:, :, -2], y, z) + \
-                tf.reduce_sum(z * self._dw[:, :, -1], 1, keepdims=True)
-            delta = y - self._bsde.g_tf(self._x[:, :, -1])
+            y = y - self.bsde.delta_t * self.bsde.f_tf(self.x[:, :, -2], y, z) + \
+                tf.reduce_sum(z * self.dw[:, :, -1], 1, keepdims=True)
+            delta = y - self.bsde.g_tf(self.x[:, :, -1])
         # use linear approximation outside the clipped range
-        self._train_loss = tf.reduce_mean(
+        self.train_loss = tf.reduce_mean(
             tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),
                      2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2))
         # f_tf also gives the true eigenfunction
-        true_init = self._bsde.g_tf(self._x[:, :, 0])
+        true_init = self.bsde.g_tf(self.x[:, :, 0])
         mask = tf.greater(tf.abs(true_init), 0.3)
-        rel_err = tf.abs((self._y_init - true_init) / true_init)
+        rel_err = tf.abs((y_init - true_init) / true_init)
         rel_err = tf.boolean_mask(rel_err, mask)
-        self._init_rel_loss = tf.reduce_mean(rel_err)
-        self._init_loss = tf.reduce_mean((true_init - self._y_init) ** 2)
+        self.init_rel_loss = tf.reduce_mean(rel_err)
+        self.init_loss = tf.reduce_mean((true_init - y_init) ** 2)
 
         # train operations
         global_step = tf.get_variable('global_step', [],
                                       initializer=tf.constant_initializer(0),
                                       trainable=False, dtype=tf.int32)
         learning_rate = tf.train.piecewise_constant(global_step,
-                                                    self._nn_config.lr_boundaries,
-                                                    self._nn_config.lr_values)
+                                                    self.nn_config.lr_boundaries,
+                                                    self.nn_config.lr_values)
         trainable_variables = tf.trainable_variables()
-        grads = tf.gradients(self._train_loss, trainable_variables)
+        grads = tf.gradients(self.train_loss, trainable_variables)
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         apply_op = optimizer.apply_gradients(zip(grads, trainable_variables),
                                              global_step=global_step, name='train_step')
-        all_ops = [apply_op] + self._extra_train_ops
-        self._train_ops = tf.group(*all_ops)
-        self._t_build = time.time()-start_time
+        all_ops = [apply_op] + self.extra_train_ops
+        self.train_ops = tf.group(*all_ops)
+        self.t_build = time.time() - start_time
 
-    def _subnetwork(self, x, num_hiddens, name, reuse=False):
-        with tf.variable_scope(name):
-            # map x to something periodic first
+
+class PeriodNet(object):
+    def __init__(self, num_hiddens, out_dim, name='period_net'):
+        self.num_hiddens = num_hiddens
+        self.out_dim = out_dim
+        self.name = name
+
+    def __call__(self, x, reuse=False):
+        dim = x.get_shape()[-1]
+        with tf.variable_scope(self.name):
             trig_bases = []
-            for i in range(self._dim):
-                trig_bases += [tf.sin(i*x), tf.cos(i*x)]
+            for i in range(dim):
+                trig_bases += [tf.sin(i * x), tf.cos(i * x)]
             trig_bases = tf.concat(trig_bases, axis=1)
-            hiddens = trig_bases
-            for i in range(1, len(num_hiddens)-1):
-                hiddens = tf.layers.dense(
-                    hiddens, num_hiddens[i], activation=tf.nn.relu, name="full%g" % (i + 1), reuse=reuse)
-            output = tf.layers.dense(
-                hiddens, num_hiddens[-1], activation=None, name="final_layer", reuse=reuse)
-        return output
+            h = trig_bases
+            for i in range(0, len(self.num_hiddens)):
+                h = tf.layers.dense(h, self.num_hiddens[i], activation=tf.nn.relu,
+                                    name="full%g" % (i + 1), reuse=reuse)
+            u = tf.layers.dense(
+                h, self.out_dim, activation=None, name="final_layer", reuse=reuse)
+            # grad = tf.gradients(u, x)[0]
+        return u
