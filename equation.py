0bc4006ef7bb6066ae0251cc50e5533896b7542c
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2019-04-01 08:57:55 -0400

    Add files via upload

--- equation.py
+++ equation.py
@@ -19,7 +19,7 @@ class Equation(object):
 
     def f_tf(self, x, y, z):
         """Generator function in the PDE."""
-        return y
+        raise NotImplementedError
 
     def g_tf(self, x):
         """Terminal condition of the PDE."""
@@ -31,34 +31,74 @@ class LaplacianEigen(Equation):
     def __init__(self, eqn_config):
         super(LaplacianEigen, self).__init__(eqn_config)
         self.sigma = np.sqrt(2.0)
-        self.eigen_array = np.reshape(np.array(eqn_config.eigen_array), [1, self.dim])
-        self.eigen = np.sum(self.eigen_array ** 2)
-#        self.eigen = tf.get_variable('eigen', shape=[1], dtype=tf.float64, initializer=None, trainable=True)
-        self._const_fac = np.sqrt(2.0) ** self.dim
-        # self.const_fac = (np.pi / 2) ** (self.dim)
 
     def sample(self, num_sample):
         dw_sample = normal.rvs(size=[num_sample,
                                      self.dim,
                                      self.num_time_interval]) * self.sqrt_delta_t
         x_sample = np.zeros([num_sample, self.dim, self.num_time_interval + 1])
-        x_sample[:, :, 0] = np.random.uniform(0.0, np.pi, size=[num_sample, self.dim])
+        x_sample[:, :, 0] = np.random.uniform(0.0, 2*np.pi, size=[num_sample, self.dim])
         for i in range(self.num_time_interval):
-            x_sample[:, :, i + 1] = x_sample[:, :, i] + self.sigma * dw_sample[:, :, i]
+            x_sample[:, :, i + 1] = x_sample[:, :, i] +  + self.sigma * dw_sample[:, :, i]
         return dw_sample, x_sample
-
-    def g_tf(self, x):
-#        return 1 + 0.1* tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True)
-        return tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True)*self._const_fac + \
-               0.05*tf.reduce_prod(tf.sin(2*self.eigen_array * x), axis=1, keepdims=True)*self._const_fac
-#        return tf.reduce_prod(x * (np.pi - x) * 2 / (np.pi ** 2), axis=1, keepdims=True) * self.const_fac
-
+    
+    def f_tf(self, x, y, z):
+        shape = tf.shape(x)
+        return tf.zeros(shape, tf.float64)
+        
     def true_z(self, x):
-        return 0
-#        prod = tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True) * self.const_fac
-#        return prod / tf.tan(self.eigen_array * x) * self.eigen_array * self.sigma
+        shape = tf.shape(x)
+        return tf.zeros(shape, tf.float64)
 
     def true_y(self, x):
         shape = tf.shape(x)
         return tf.ones(shape, tf.float64)
-#        return tf.reduce_prod(tf.sin(self.eigen_array * x), axis=1, keepdims=True)*self._const_fac
+
+
+class FokkerPlanckEigen(Equation):
+    # eigenvalue problem for Fokker Planck operator on squares [0, 2pi]^d
+    def __init__(self, eqn_config):
+        super(FokkerPlanckEigen, self).__init__(eqn_config)
+        self.sigma = np.sqrt(2.0)
+        
+    def v(self, x):
+        # the size of x is (num_sample, dim)
+        return tf.cos(x[:,0])
+    
+    def grad_v(self, x):
+        temp = -np.sin(x)
+        temp[:,1:self.dim-1] = 0
+        return temp
+    
+    def laplician_v(self, x):
+        return -tf.cos(x[:,0])
+    
+    def f_tf(self, x, y, z):
+        return -y * tf.cos(x[:,0])
+#        return y * self.laplician_v(self, x)
+
+    def sample(self, num_sample):
+        dw_sample = normal.rvs(size=[num_sample,
+                                     self.dim,
+                                     self.num_time_interval]) * self.sqrt_delta_t
+        x_sample = np.zeros([num_sample, self.dim, self.num_time_interval + 1])
+        #for now X_0 is uniformly sampled
+        x_sample[:, :, 0] = np.random.uniform(0.0, 2*np.pi, size=[num_sample, self.dim])
+        for i in range(self.num_time_interval):
+            # wrong
+            x_sample[:, :, i + 1] = x_sample[:, :, i] - \
+            self.grad_v(x_sample[:, :, i])*self.delta_t + self.sigma * dw_sample[:, :, i]
+#            x_sample[:, :, i + 1] = x_sample[:, :, i] + \
+#            self.grad_v(self, x_sample[:, :, i])*self.delta_t + self.sigma * dw_sample[:, :, i]
+        return dw_sample, x_sample    
+    
+    def true_y(self, x):
+        return tf.exp(-tf.cos(x[:,0]))
+#        return tf.exp(-self.v(self, x))
+        
+    def true_z(self, x):
+        shape = tf.shape(x)
+        x1 = tf.concat([tf.reshape(x[:,0],[shape[0],1]), tf.zeros(shape-[0, 1],tf.float64)], 1)
+        return tf.math.sin(x1) * tf.math.exp(-tf.math.cos(x1))
+
+    
