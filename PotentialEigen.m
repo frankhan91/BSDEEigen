6ae72f305a97f366fb6124b64454bc422712c70f
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2019-05-04 09:49:41 -0400

    Add files via upload

--- PotentialEigen.m
+++ PotentialEigen.m
@@ -7,7 +7,7 @@ N = 20; % truncation
 a = 1:N; a = a.^2;
 b = [a(end:-1:1), 0, a];
 e = diag(ones(2*N,1),1);
-A = diag(2*b) - e - e';
+A = diag(2*b) + e + e';
 [V,D] = eig(A);
 eigen = D(1,1)/2 ;
 coef = V(:,1)';
